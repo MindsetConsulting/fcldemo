ecbffe5fbaedb600f706336dd3f1f5cd42a390c2
Curtis Kettler <curtiskettler@Curtiss-MacBook-Pro.local>
2022-02-17 09:01:11 -0600

    add employee working correctly

--- fcldemo/webapp/controller/Master.controller.js
+++ fcldemo/webapp/controller/Master.controller.js
@@ -5,8 +5,12 @@ sap.ui.define([
 	"sap/ui/model/FilterOperator",
 	"sap/ui/model/Sorter",
 	"sap/m/MessageBox",
-    "sap/f/library"
-], function (JSONModel, Controller, Filter, FilterOperator, Sorter, MessageBox, fioriLibrary) {
+    "sap/f/library",
+	"sap/m/Dialog",
+	"sap/m/DialogType",
+	"sap/m/Button",
+	"sap/m/ButtonType"
+], function (JSONModel, Controller, Filter, FilterOperator, Sorter, MessageBox, fioriLibrary, Dialog, DialogType, Button, ButtonType) {
 	"use strict";
 
 	return Controller.extend("fcldemo.fcldemo.controller.Master", {
@@ -29,7 +33,110 @@ sap.ui.define([
 		},
 
 		onAdd: function () {
-			MessageBox.information("This functionality is not ready yet.", {title: "Aw, Snap!"});
+			// Get the Model in the view
+			var oModel = this.getView().getModel("employee");
+			// Get the Number of records in the OData Employees
+			var EmployeesNumber = oModel.getProperty("/Employees").length;
+			// Populate the new Employee ID
+			var NewEmployeeID = EmployeesNumber + 1;
+			// Display a popup to Insert the new Employee First Name and Employee Last Name
+			// call Dialog popup
+			var dialog = new sap.m.Dialog({
+				title: "Add Employee",
+				type: "Message",
+				content: [new sap.ui.layout.HorizontalLayout({
+					content: [new sap.ui.layout.VerticalLayout({
+						width: "150px",
+						content: [
+							new sap.m.Label({
+								text: "Id"
+							}),
+							new sap.m.Input("Id", {
+								value: NewEmployeeID,
+								editable: false
+							}),
+							new sap.m.Label({
+								text: "Full Name"
+							}),
+							new sap.m.Input("FullName"),
+							new sap.m.Label({
+								text: "Department"
+							}),
+							new sap.m.Input("Department"),
+							new sap.m.Label({
+								text: "Title"
+							}),
+							new sap.m.Input("Title"),
+							new sap.m.Label({
+								text: "Role"
+							}),
+							new sap.m.Input("Role"),
+							new sap.m.Label({
+								text: "Industries"
+							}),
+							new sap.m.Input("Industries"),
+							new sap.m.Label({
+								text: "Direct Report"
+							}),
+							new sap.m.Input("DirectReport"),
+							new sap.m.Label({
+								text: "Start Date"
+							}),
+							new sap.m.Input("StartDate"),
+							new sap.m.Label({
+								text: "Email"
+							}),
+							new sap.m.Input("Email")
+						]
+					})]
+				})],
+				beginButton: new sap.m.Button({
+					text: "Save",
+					press: function() {
+						// Read the new Employee values
+						var NewEmployeeID = sap.ui.getCore().byId("Id").getValue();
+						var NewEmployeeFullName = sap.ui.getCore().byId("FullName").getValue();
+						var NewEmployeeDepartment = sap.ui.getCore().byId("Department").getValue();
+						var NewEmployeeTitle = sap.ui.getCore().byId("Title").getValue();
+						var NewEmployeeRole = sap.ui.getCore().byId("Role").getValue();
+						var NewEmployeeIndustries = sap.ui.getCore().byId("Industries").getValue();
+						var NewEmployeeDirectReport = sap.ui.getCore().byId("DirectReport").getValue();
+						var NewEmployeeStartDate = sap.ui.getCore().byId("StartDate").getValue();
+						var NewEmployeeEmail = sap.ui.getCore().byId("Email").getValue();
+						// Create Object
+						var oEmployee = {};
+						oEmployee = {
+							"dD": NewEmployeeID,
+							"FullName": NewEmployeeFullName,
+							"Department": NewEmployeeDepartment,
+							"Title": NewEmployeeTitle,
+							"Role": NewEmployeeRole,
+							"Industries": NewEmployeeIndustries,
+							"DirectReport": NewEmployeeDirectReport,
+							"StartDate": NewEmployeeStartDate,
+							"Email": NewEmployeeEmail
+						};
+						//Get the OData 
+						var oEmployees = oModel.getProperty("/Employees");
+						// Add the new record to the Odata
+						oEmployees.push(oEmployee);
+						oModel.setProperty("/Employees", oEmployees);
+						// Close the popup
+						dialog.close();
+					}
+				}),
+				endButton: new sap.m.Button({
+					text: "Cancel",
+					press: function() {
+						dialog.close();
+					}
+				}),
+				afterClose: function() {
+					dialog.destroy();
+				}
+			});
+			dialog.open();
+
 		},
 
 		onSort: function () {
